35c5d68cc56753b20a70ccd22ed689f2ef0b4810
busisatya@gmail.com <busisatya@gmail.com>
2023-06-11 13:30:48 -0400

    ArrayDestructuring

---
+++ scripts/4arraydestructuring.js
@@ -0,0 +1,19 @@
+"use strict";
+//This will work just fine
+// let employee = ["Sheldon", "Cooper", "Male"]
+// let [fname, lname, gender] = employee;
+// Now let's say
+// let employee = ["Sheldon", "Cooper"]
+// let [fname, lname, gender] = employee;
+//Here gender is undefined
+// let employee = ["Sheldon", "Cooper"]
+// //WE can use default params
+// let [fname, lname, gender="Female"] = employee;
+let employee = ["Sheldon", "Cooper", "Male"];
+//The rest of parameters in employee are assigned to pars as an array
+let [fname, ...pars] = employee;
+// If we don't want any of firstname and lastname, we can simply do
+let [, , gender] = employee; //Gives MALE
+console.log(fname);
+console.log(gender);
+//# sourceMappingURL=4arraydestructuring.js.map
