77a56cd7821279cde1ece062f11310706da8720e
busisatya@gmail.com <busisatya@gmail.com>
2023-06-10 21:38:06 -0400

    This keyword, with arrow function, without arrow function

---
+++ 2this.ts
@@ -0,0 +1,34 @@
+//This code stub beloe gets this context from the function
+// var employee = {
+//     name:"Busi",
+//     greet: function(){
+//         setTimeout(function(){
+//             console.log(this.name);
+//         },1000);
+//     }
+// };
+
+
+//Solution 1
+// var employee = {
+//     name:"Busi",
+//     greet: function(){
+//         var self = this;
+//         setTimeout(function(){
+//             console.log(self.name);
+//         },1000);
+//     }
+// };
+
+
+//Solution 1
+// This gets context from outside.
+var employee = {
+    name:"Busi",
+    greet: function(){
+        setTimeout(() => console.log(this.name),1000);
+    }
+};
+
+
+employee.greet();
